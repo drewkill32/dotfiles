504aecbda74ff2a79a2c857d918ed1c9e0bc4310
Drew Killion <adk@killion.me>
2022-11-29 20:26:22 -0500

    fixing symbolic links

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,1 +1,26 @@
-C:/Users/adk/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+(@(& 'C:/Users/adk/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\adk\.oh-my-posh\oh-my-posh.json' --print) -join "`n") | Invoke-Expression
+
+if ($host.Name -eq 'ConsoleHost') {
+  Import-Module PSReadLine
+}
+
+Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -PredictionSource History -WarningAction Ignore
+Set-PSReadLineOption -PredictionViewStyle ListView -WarningAction Ignore
+Set-PSReadLineOption -EditMode Windows -WarningAction Ignore
+
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Set-Alias -Name touch -Value New-Item
+
+
