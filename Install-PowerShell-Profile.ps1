4c4ac7efcdf9cf997659719f8b5003117f6cfe46
Drew Killion <DKillion@blueModus.com>
2023-07-27 13:40:40 -0400

    updating profile and oh my posh

--- Install-PowerShell-Profile.ps1
+++ Install-PowerShell-Profile.ps1
@@ -2,6 +2,29 @@ $ScriptPath = Join-Path $PSScriptRoot -ChildPath Microsoft.Powershell_profile.ps
 $AddProfileStatement = @("& $($ScriptPath)")
 
 
+function Update-Paths {
+    $paths = 'Machine', 'User', 'Process' |
+    ForEach-Object {
+      ([System.Environment]::GetEnvironmentVariable('Path', $_)) -split ';'
+    } |
+    Select-Object -Unique
+    $Env:PATH = $paths -join ';'
+}
+  
+
+if (!(Get-Command oh-my-posh -ErrorAction Ignore)) {
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+    Update-Paths
+}
+
+if (!(Get-Module -Name PSReadLine -ListAvailable | Where-Object Version -GE 2.2.6)) {
+    Install-Module PSReadLine -Scope CurrentUser -MinimumVersion 2.2.6 -Force
+} 
+  
+if (Get-Module -Name PSReadLine | Where-Object Version -GE 2.2.6) {
+    Import-Module PSReadLine -MinimumVersion 2.2.6 -Force -NoClobber
+}
+
 
 $content = Get-Content $PROFILE
 if ($content -notcontains $AddProfileStatement) {
--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,5 +1,5 @@
 function Update-Paths {
-  $paths = 'Machine', 'User' |
+  $paths = 'Machine', 'User', 'Process' |
   ForEach-Object {
     ([System.Environment]::GetEnvironmentVariable('Path', $_)) -split ';'
   } |
@@ -7,42 +7,38 @@ function Update-Paths {
   $Env:PATH = $paths -join ';'
 }
 
-if ((Get-PSRepository PSGallery).InstallationPolicy -ne "Trusted") {
-  Set-PSRepository PSGallery -InstallationPolicy Trusted
-}
 
-if (!(Get-Command oh-my-posh -ErrorAction Ignore)) {
-  winget install JanDeDobbeleer.OhMyPosh -s winget
-  Update-Paths
-}
+
 
 $poshProfile = Join-Path $PSScriptRoot -ChildPath oh-my-posh.json
 
 
 (@(& oh-my-posh init pwsh --config $poshProfile --print) -join "`n") | Invoke-Expression
 
+
 if ($host.Name -eq 'ConsoleHost') {
-  Import-Module PSReadLine
+
 }
+
 
 if (!(Get-Module -ListAvailable -Name Terminal-Icons)) {
   Install-Module Terminal-Icons -Scope CurrentUser
   Import-Module Terminal-Icons
 }
 
+
 if (!(Get-Module -ListAvailable -Name Posh-Git)) {
   Install-Module Posh-Git -Scope CurrentUser
   Import-Module Posh-Git
 } 
+$env:POSH_GIT_ENABLED = $true
 
-if (!(Get-Module -Name PSReadLine -ListAvailable | Where-Object Version -ge 2.2.6)) {
-  Install-Module PSReadLine -Scope CurrentUser -MinimumVersion 2.2.6 -Force
-  Import-Module PSReadLine
-} 
+
 
 Set-PSReadLineOption -PredictionSource History -WarningAction Ignore
 Set-PSReadLineOption -PredictionViewStyle ListView -WarningAction Ignore
 Set-PSReadLineOption -EditMode Windows -WarningAction Ignore
+
 
 
 # Alaises
@@ -57,6 +53,7 @@ $alaises | ForEach-Object {
     Set-Alias -Name $_.Name -Value $_.Value
   }
 }
+
 
 
 
